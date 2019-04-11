dbaaf2b34b7af0cbd078183e63fbb119c2322504
Chinseok Lee <me@askcompany.kr>
2019-04-11 11:37:31 +0900

    shop 앱 생성/등록하고, 모델 생성/마이그레이션

---
+++ shop/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    desc = models.TextField()
+    price = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
