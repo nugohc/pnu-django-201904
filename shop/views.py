28fc735e0e7fb11146e57e882edc4b95d2aef9b5
Chinseok Lee <me@askcompany.kr>
2019-04-11 17:00:06 +0900

    CBV 예시 추가

--- shop/views.py
+++ shop/views.py
@@ -1,10 +1,14 @@
+# from django.views.generic import ListView
 from django.shortcuts import render
 from .models import Item
 
 
+# item_list = ListView.as_view(model=Item)
+
+
 def item_list(request):
     # DB로부터 모든 Item List를 가져올 예정
-    qs  = Item.objects.all()  # QuerySet 타입
+    qs = Item.objects.all()  # QuerySet 타입
     # 템플릿 파일 위치 : shop/templates/shop/item_list.html
     return render(request, 'shop/item_list.html', {
         'item_list': qs,
