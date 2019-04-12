28d5f606a8c61e3267eed3575f957f1c53a7d138
Chinseok Lee <me@askcompany.kr>
2019-04-12 14:17:58 +0900

    Heroku 배포 준비

--- pnu/settings.py
+++ pnu/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@t+z^3&1$)t(ur%nv1bd4%a_g+93tolh=n&z-!bkelofa%cesg'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -120,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+import django_heroku
+django_heroku.settings(locals())
